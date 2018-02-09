c342b293022ea2b1734c91735e6fdc3347a33a06
Mathieu Sylvestre <mathieusylvestre17@gmail.com>
2018-02-09 17:16:13 -0500

    ajout du cote client pour le login avec js,html et css

---
+++ application/views/atterrissage/connexion-form.php
@@ -0,0 +1,14 @@
+<section id="connexion-form">
+	<div class="form-group">
+	  <label for="username">Votre username</label>
+	  <input type="text" class="form-control" id="username" aria-describedby="username">
+	</div>
+	<div class="form-group">
+	  <label for="password">Votre mot de passe</label>
+	  <input type="password" class="form-control" id="password">
+	</div>
+	<div class="d-flex flex-wrap justify-content-end">
+		<a href="#" id="cancel" class="d-block py-2 px-3 text-white">Annuler</a>
+		<button id="connexion-submit" class="btn btn-primary">Connexion</button>
+	</div>
+</section>
