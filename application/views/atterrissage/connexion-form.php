72f942840372948da9eb6639b714f6975b67e9f2
leilasalhi <s.leila94@gmail.com>
2018-02-11 15:21:05 -0500

    ajout de model usager et modifs dans les vues connexion et inscription

--- application/views/atterrissage/connexion-form.php
+++ application/views/atterrissage/connexion-form.php
@@ -1,14 +1,50 @@
+<?php
+    echo form_open('Usagers/connexion');
+?>
+
 <section id="connexion-form">
 	<div class="form-group">
-	  <label for="username">Votre username</label>
-	  <input type="text" class="form-control" id="username" aria-describedby="username">
+        <?php
+        echo form_label('Votre username', 'nomUsager');
+        $nomUsager= array(
+
+            'name'=>'nomUsager',
+            'id'=>'username',
+            'class'=>'form-control'
+
+        );
+        echo form_input($nomUsager);
+        ?>
+	  <!--<label for="username">Votre username</label>
+	  <input type="text" class="form-control" name="nomUsager" id="username" aria-describedby="username">-->
 	</div>
 	<div class="form-group">
-	  <label for="password">Votre mot de passe</label>
-	  <input type="password" class="form-control" id="password">
+        <?php
+        echo form_label('Votre mot de passe', 'password');
+        $motDePasse= array(
+            'type'=>'password',
+            'name'=>'motDePasse',
+            'id'=>'password',
+            'class'=>'form-control'
+        );
+        echo form_input($motDePasse);
+        ?>
+	  <!--<label for="password">Votre mot de passe</label>
+	  <input type="password" class="form-control" name="motDePasse" id="password">-->
 	</div>
 	<div class="d-flex flex-wrap justify-content-end">
 		<a href="#" id="cancel" class="d-block py-2 px-3 text-white">Annuler</a>
-		<button id="connexion-submit" class="btn btn-primary">Connexion</button>
+		<!--<button id="connexion-submit" class="btn btn-primary">Connexion</button>-->
+        <?php 
+        $button= array(
+            'id'=>'connexion-submit',
+            'class'=>'btn btn-primary',
+            'value'=>'Connexion'
+        );
+        echo form_submit($button);
+        ?>
 	</div>
 </section>
+<?php
+    echo form_close();
+?>
--- application/views/atterrissage/inscription-form.php
+++ application/views/atterrissage/inscription-form.php
@@ -1,30 +1,88 @@
+<?php
+    echo form_open('Usagers/inscription');
+?>
 <section id="inscription-form">
 	<div class="form-group">
-	  <label for="username">Choisissez votre username</label>
-	  <input type="text" class="form-control" id="username" aria-describedby="username" placeholder="Unique, il ne pourra être changé">
+        <?php
+        echo form_label('Choisissez votre username', 'username');
+        $nomUsager= array(
+
+            'name'=>'nomUsager',
+            'id'=>'username',
+            'class'=>'form-control',
+            'placeholder'=>'Unique, il ne pourra être changé'
+
+        );
+        echo form_input($nomUsager);
+        ?>
+	  <!--<label for="username">Choisissez votre username</label>
+	  <input type="text" class="form-control" name="nomUsager" id="username" aria-describedby="username" placeholder="Unique, il ne pourra être changé">-->
 	  <div class="valid-feedback"></div>
 	  <div class="invalid-feedback"></div>
 	</div>
 	<div class="form-group">
-	  <label for="password">Votre mot de passe</label>
-	  <input type="password" class="form-control" id="password">
+        <?php
+        echo form_label('Votre mot de passe', 'password');
+        $motDePasse= array(
+            'type'=>'password',
+            'name'=>'motDePasse',
+            'id'=>'password',
+            'class'=>'form-control'
+        );
+        echo form_input($motDePasse);
+        ?>
+	  <!--<label for="password">Votre mot de passe</label>
+	  <input type="password" class="form-control" id="password" name="motDePasse">-->
 	  <div class="valid-feedback"></div>
 	  <div class="invalid-feedback"></div>
-  </div>
+    </div>
 	<div class="form-group">
-	  <label for="password_confirm">Confirmez le mot de passe</label>
-	  <input type="password" class="form-control" id="password_confirm">
+        <?php
+        echo form_label('Confirmez le mot de passe', 'password_confirm');
+        $motDePasse= array(
+
+            'type'=>'password',
+            'name'=>'motDePasse',
+            'id'=>'password_confirm',
+            'class'=>'form-control'
+        );
+        echo form_input($motDePasse);
+        ?>
+        <!-- <label for="password_confirm">Confirmez le mot de passe</label>
+	  <input type="password" class="form-control" id="password_confirm">-->
 	  <div class="valid-feedback"></div>
 	  <div class="invalid-feedback"></div>
 	</div>
 	<div class="form-group">
-	  <label for="password">Indiquez votre courriel</label>
-	  <input type="mail" class="form-control" id="courriel">
+       <?php
+        echo form_label('Indiquez votre courriel', 'courriel');
+        $motDePasse= array(
+
+            'type'=>'mail',
+            'name'=>'courriel',
+            'id'=>'courriel',
+            'class'=>'form-control'
+        );
+        echo form_input($motDePasse);
+        ?> 
+<!--	  <label for="courriel">Indiquez votre courriel</label>
+	  <input type="mail" class="form-control" id="courriel" name="courriel">-->
 	  <div class="valid-feedback"></div>
 	  <div class="invalid-feedback"></div>
 	</div>
 	<div class="d-flex flex-wrap justify-content-end">
 		<a href="#" id="cancel" class="d-block py-2 px-3 text-white">Annuler</a>
-		<button id="inscription-submit" class="btn btn-primary">M'inscrire</button>
+		<!--<button id="inscription-submit" class="btn btn-primary">M'inscrire</button>-->
+        <?php 
+        $button= array(
+            'id'=>'connexion-submit',
+            'class'=>'btn btn-primary',
+            'value'=>"M'inscrire"
+        );
+        echo form_submit($button);
+        ?>
 	</div>
 </section>
+<?php
+    echo form_close();
+?>
