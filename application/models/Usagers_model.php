ee3050aeceee602fea619b2dd0dd801def00d549
unknown <peace@gmx.fr>
2018-02-16 10:29:57 -0500

    upstream attnd commentaire usagers odel et moyen contact

--- application/models/Usagers_model.php
+++ application/models/Usagers_model.php
@@ -83,36 +83,58 @@ class Usagers_model extends CI_Model {
     }
     return $reponse;
   }
-
+  
+  /**
+  * inscrire un nouveau utilisateur dans la base de donnée
+  *
+  * @return     boolean  ( description_of_the_return_value )
+  */
   public function modifier_usager($nomUsager, $Nom, $Prenom, $motDePasse, $Adresse, $typePaiem){//usager modifie ses données 
       //ce tableau contient toutes les nouvelles données, récupérées du formulaire de modification
-      $data = array (
-        "Nom" => $Nom,
-        "Prenom" => $Prenom,
-        "motDePasse" =>  password_hash($motDePasse, PASSWORD_DEFAULT),
-        "Adresse" => $Adresse,
-        "typePaiem" => $typePaiem,            
-      );
-      $this->db->where('nomUsager', $nomUsager);
-      $this->db->update('Usager', $data);
+    $data = array (
+      "Nom" => $Nom,
+      "Prenom" => $Prenom,
+      "motDePasse" =>  password_hash($motDePasse, PASSWORD_DEFAULT),
+      "Adresse" => $Adresse,
+      "typePaiem" => $typePaiem,            
+    );
+    $this->db->where('nomUsager', $nomUsager);
+    $this->db->update('Usager', $data);
     
-    }
-    
+  }
+
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $nomUsager  The nom usager
+     */
     public function supprimer_usager($nomUsager){//suppression d'un usager
-      $this->db->where('nomUsager', $nomUsager);
-      $this->db->delete('Usager');
-    }
-    
-    public function ValiderInvalider_usager($nomUsager, $estValide){//valider invalider usager 
+    $this->db->where('nomUsager', $nomUsager);
+    $this->db->delete('Usager');
+  }
+
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $nomUsager  The nom usager
+     * @param      <type>  $estValide  The est valide
+     */
+    public function changeStatusValide_usager($nomUsager, $estValide){//valider invalider usager 
       //ce tableau contient toutes les nouvelles données, récupérées du formulaire de modification
       $data = array (
         "estValide" => $estValide,          
       );
       $this->db->where('nomUsager', $nomUsager);
       $this->db->update('Usager', $data);
-    
+
     }
     
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $nomUsager  The nom usager
+     * @param      <type>  $estValide  The est valide
+     */
     public function BannirInbannir_usager($nomUsager, $estValide){//bannir inbannir usager 
       //ce tableau contient toutes les nouvelles données, récupérées du formulaire de modification
       $data = array (
@@ -120,22 +142,36 @@ class Usagers_model extends CI_Model {
       );
       $this->db->where('nomUsager', $nomUsager);
       $this->db->update('Usager', $data);
-    
+
     }
     
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $Role   The role
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
     public function obtenir_usagersParRole($Role){//affichage de tout les usagers ayant un role précis
       $this->db->select('*');
       $this->db->from('Usager');
       $this->db->join('Role', 'Usager.idRole = Role.idRole');
       $this->db->where('Role', $Role);
       $this->db->order_by('nomUsager', 'DESC');//trie par le nomUsager
-    
+
       $query = $this->db->get();
       
       //tableau de resultats
       return $query->result_array();
     }
     
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $estBanni  The est banni
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
     public function obtenir_usagersParBanniInbanni($estBanni){//affichage de tout les usagers banni ou pas banni
       $query = $this->db->get_where("Usager", array("estBanni" => $estBanni));
       
@@ -144,6 +180,13 @@ class Usagers_model extends CI_Model {
     }
     
     
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $estValide  The est valide
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
     public function obtenir_usagersParValideInvalide($estValide){//affichage de tout les usagers valide ou pas valide
       $query = $this->db->get_where("Usager", array("estValide" => $estValide));
       
@@ -151,6 +194,13 @@ class Usagers_model extends CI_Model {
       return $query->result_array();
     }
     
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $typePaiem  The type paiem
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
     public function obtenir_usagersParTypePaiement($typePaiem){//affichage de tout les usagers ayant un type de paiement précis
       $query = $this->db->get_where("Usager", array("typePaiem" => $typePaiem));
       
@@ -158,6 +208,13 @@ class Usagers_model extends CI_Model {
       return $query->result_array();
     }
     
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $Validateur  The validateur
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
     public function obtenir_usagersParValidateur($Validateur){//affichage de tout les usagers ayant été validés par un administrateur précis
       $query = $this->db->get_where("Usager", array("Validateur" => $Validateur));
       
@@ -165,57 +222,69 @@ class Usagers_model extends CI_Model {
       return $query->result_array();
     }
 
+    /**
+     * { function_description }
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
     public function obtenir_usagers(){//affichage de tout les usagers pour l'admin ,ordre par defaut sur la clé primaire nomUsager
-      $this->db->select('*, count(Appartement.idAppart) as NbreAppart, count(Location.idLocation) as NbreLocat');
-      $this->db->from('Usager');
-      $this->db->join('Mode_paiement', 'Usager.typePaiem = Mode_paiement.typePaiem');
-      $this->db->join('Role', 'Usager.idRole = Role.idRole');
-      $this->db->join('Moyen_contact', 'Usager.nomUsager = Moyen_contact.nomUsager', 'right');
-      $this->db->join('Location', 'Usager.nomUsager = Location.Locataire', 'right');
-      $this->db->join('Appartement', 'Usager.nomUsager = Appartement.Proprietaire', 'right');
-      $this->db->group_by("Usager.nomUsager");
-      switch ($this->input->post('var_Trie',true))
-      {
-        case "Role":
+    $this->db->select('*, count(Appartement.idAppart) as NbreAppart, count(Location.idLocation) as NbreLocat');
+    $this->db->from('Usager');
+    $this->db->join('Mode_paiement', 'Usager.typePaiem = Mode_paiement.typePaiem');
+    $this->db->join('Role', 'Usager.idRole = Role.idRole');
+    $this->db->join('Moyen_contact', 'Usager.nomUsager = Moyen_contact.nomUsager', 'right');
+    $this->db->join('Location', 'Usager.nomUsager = Location.Locataire', 'right');
+    $this->db->join('Appartement', 'Usager.nomUsager = Appartement.Proprietaire', 'right');
+    $this->db->group_by("Usager.nomUsager");
+    switch ($this->input->post('var_Trie',true))
+    {
+      case "Role":
           $this->db->order_by('Role', 'DESC');//trie par le role d'utilisateur admin,membre,super admin
           break;
-        case "typePaiem":
+          case "typePaiem":
           $this->db->order_by('typePaiem', 'DESC');//trie par type de paiement choisi
           break;
-        case "NbreAppart":
+          case "NbreAppart":
           $this->db->order_by('NbreAppart', 'DESC');//trie par nombre d'appartements possédés
           break;
-        case "NbreLocat":
+          case "NbreLocat":
           $this->db->order_by('NbreLocat', 'DESC');//trie par nombre de locations effectuées
           break;
-        case "dateCreationCompte":
+          case "dateCreationCompte":
           $this->db->order_by('dateCreationCompte', 'ASC');//trie par date de creation de compte ordre croissant
           break;
-        default:
+          default:
           $this->db->order_by('nomUsager', 'DESC');//par defaut trier sur nomUsager
-      }
-      
-      $query = $this->db->get();
-      
-      //tableau de resultats
-      return $query->result_array();
-    }
+        }
 
-    
-    public function obtenir_usagerParNomUsager($nomUsager){//affichage de toute les informations d'un usager précis
-      $this->db->select('*');
-      $this->db->from('Usager');
-      $this->db->join('Mode_paiement', 'Usager.typePaiem = Mode_paiement.typePaiem');
-      $this->db->join('Role', 'Usager.idRole = Role.idRole');
-      $this->db->join('Moyen_contact', 'Usager.nomUsager = Moyen_contact.nomUsager', 'right');
-      $this->db->join('Location', 'Usager.nomUsager = Location.Locataire', 'right');
-      $this->db->join('Appartement', 'Usager.nomUsager = Appartement.Proprietaire', 'right');
-      $this->db->where('nomUsager', $nomUsager);
-      
-      $query = $this->db->get();
-      
+        $query = $this->db->get();
+
       //tableau de resultats
-      return $query->result_array();
-    }
+        return $query->result_array();
+      }
+
+
+    /**
+     * { function_description }
+     *
+     * @param      <type>  $nomUsager  The nom usager
+     *
+     * @return     <type>  ( description_of_the_return_value )
+     */
+    public function obtenir_usagerParNomUsager($nomUsager){//affichage de toute les informations d'un usager précis
+    $this->db->select('*');
+    $this->db->from('Usager');
+    $this->db->join('Mode_paiement', 'Usager.typePaiem = Mode_paiement.typePaiem');
+    $this->db->join('Role', 'Usager.idRole = Role.idRole');
+    $this->db->join('Moyen_contact', 'Usager.nomUsager = Moyen_contact.nomUsager', 'right');
+    $this->db->join('Location', 'Usager.nomUsager = Location.Locataire', 'right');
+    $this->db->join('Appartement', 'Usager.nomUsager = Appartement.Proprietaire', 'right');
+    $this->db->where('nomUsager', $nomUsager);
+
+    $query = $this->db->get();
+
+      //tableau de resultats
+    return $query->result_array();
+  }
 
 }//Fin de la classe
