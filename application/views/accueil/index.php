1f759ba5ee45c8513a1cdcb0c8f10d69b20ae2ef
yamanibhk <peace@gmx.fr>
2018-02-27 17:24:52 -0500

    conflit2

--- application/views/accueil/index.php
+++ application/views/accueil/index.php
@@ -1,12 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<!--<link rel="stylesheet" type="text/css" href="<?=base_url();?>css/accueil/stylesheet.css">-->
+<link rel="stylesheet" type="text/css" href="<?=base_url();?>css/accueil/stylesheet.css">
 <link rel="stylesheet" type="text/css" href="<?=base_url();?>css/accueil/font/flaticon.css">
-<!--<script type='text/javascript' src="<?=base_url();?>js/accueil/script.js"></script>-->
+<script type='text/javascript' src="<?=base_url();?>js/accueil/script.js"></script>-->
 <!--source https://bootsnipp.com/snippets/aMnXx-->
 <script src='//production-assets.codepen.io/assets/editor/live/console_runner-079c09a0e3b9ff743e39ee2d5637b9216b3545af0de366d4b9aad9dc87e26bfd.js'></script>
-<!--<script src='//production-assets.codepen.io/assets/editor/live/events_runner-73716630c22bbc8cff4bd0f07b135f00a0bdc5d14629260c3ec49e5606f98fdd.js'></script>-->
 <script src='//production-assets.codepen.io/assets/editor/live/css_live_reload_init-2c0dc5167d60a5af3ee189d570b1835129687ea2a61bee3513dee3a50c115a77.js'></script>
 <script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script>
 <link rel="shortcut icon" type="image/x-icon" href="//production-assets.codepen.io/assets/favicon/favicon-8ea04875e70c4b0bb41da869e81236e54394d63638a1ef12fa558a4a835f1164.ico" />
